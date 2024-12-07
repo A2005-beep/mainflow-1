7f5f044feab840d8280c4cdcde2c0c1cf7ab917a
Afifa Ahmed <afifaahmed218@gmail.com>
2024-12-07 09:04:54 -0800

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,22 @@
+my_list = [1, 2, 3, 4, 5]
+print("Original list:", my_list)
+my_list.append(66)
+print("After adding 66 to the original list:", my_list.append)
+my_list.remove(3)
+print("After removing 3 from the original list:", my_list.remove)
+my_list.pop()
+print("After popping the original list:",my_list.pop)
+my_dictionary = {"Name": "Afifa", "Age": "19", "City": "Hyderabad"}
+print("Original dictionary:", my_dictionary)
+my_dictionary["Hobby"] = "Sleeping"
+print("After adding Hobby:", my_dictionary)
+my_dictionary["Age"] = "22"
+print("After updating the age:", my_dictionary)
+del my_dictionary["City"]
+print("After deleting City:", my_dictionary)
+my_set ={10,20,22,34,56}
+print("Original Set:", my_set)
+my_set.add(6)
+print("After adding a new element:", my_set)
+my_set.remove(20)
+print("After removing an element:", my_set)
